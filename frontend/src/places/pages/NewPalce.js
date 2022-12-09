38c8cdf4c5fbac69134a3dcbeaa2508774000c41
Peter Maged <petermaged222@gmail.com>
2022-12-10 01:49:45 +0200

    adding form validation

--- frontend/src/places/pages/NewPalce.js
+++ frontend/src/places/pages/NewPalce.js
@@ -1,9 +1,11 @@
 import './NewPlace.css';
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from '../../shared/utils/Validatiors';
 
 const NewPlace = () => {
+  const errorText = "Please enter a valid title.";
   return <form className="place-form">
-    <Input element="input" type="text" label="Title" />
+    <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText={errorText} />
   </form>;
 };
 
